5cacaa37565f6bd99666f72056b68a0bb0c9b797
Jonas Helfer <jonas@helfer.email>
2017-08-27 11:29:58 -0700

    Implement static layout

--- client/src/App.js
+++ client/src/App.js
@@ -7,42 +7,64 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <div className="App-body">
-
-          <div className="ui card centered">
-            <div className="content"><div className="header">
-              <div className="zetteli-date">
-                <span className="date">Fr. 25.8.2017 09:50</span>
+        <div className="ui text container">
+          <div className="ui pointing secondary menu">
+            <a className="active item">
+              Home
+            </a>
+            <a className="item">
+              Archive
+            </a>
+            <a className="item">
+              Settings
+            </a>
+            <div className="right menu">
+              <div className="item">
+                <div className="ui transparent icon input">
+                  <input type="text" placeholder="Search..." />
+                  <i className="search link icon"></i>
+                </div>
               </div>
-              <div className="zetteli-tags">
-                <span className="tag">log</span>
-                <span className="tag">personal</span>
-              </div>
-            </div></div>
-            <div className="zetteli-body">
+            </div>
+          </div>
+          <div className="ui card centered fluid">
+            <div className="extra content">
+              <span className="left floated">
+                Friday, 8/25/2017 09:50
+              </span>
+              <span className="right floated">
+                <a className="ui label small">
+                  log
+                </a>
+                <a className="ui label small">
+                  personal
+                </a>
+              </span>
+            </div>
+            <div className="left aligned content">
               <p>This is the first hard-coded zetteli in history!</p>
             </div>
           </div>
 
-          <div className="ui card centered">
-            <div className="zetteli-header">
-              <div className="zetteli-date">
-                <span className="date">Fr. 25.8.2017 09:54</span>
-              </div>
-              <div className="zetteli-tags">
-                <span className="tag">log</span>
-                <span className="tag">personal</span>
-              </div>
+          <div className="ui card centered fluid">
+            <div className="extra content">
+                <span className="left floated">Friday, 8/25/2017 09:54</span>
+              <span className="right floated">
+                <span className="ui label small">
+                  log
+                </span>
+                <span className="ui label small">
+                  personal
+                </span>
+              </span>
             </div>
-            <div className="zetteli-body">
-              <p contenteditable="true">This zetteli is currently being edited!</p>
+            <div className="left aligned content">
+              <p contentEditable="true">This zetteli is currently being edited!</p>
             </div>
           </div>
-          <button className="add-zetteli button">+</button>
+          <button className="ui circular icon button">
+            <i className="plus icon"></i>
+          </button>
         </div>
       </div>
     );
