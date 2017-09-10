9b0561e9068791671bd152c323e3704dafcf00bf
Jonas Helfer <jonas@helfer.email>
2017-09-09 19:26:35 -0700

    Use enzyme.render and sketch additional tests

--- client/src/components/EditableText.test.tsx
+++ client/src/components/EditableText.test.tsx
@@ -6,7 +6,19 @@ describe('EditableText', () => {
     it('renders the text', () => {
         const testText = 'Hi there!';
         const oc = jest.fn();
-        const editableText = enzyme.mount(<EditableText text={testText} onChange={oc}/>);
+        const editableText = enzyme.render(<EditableText text={testText} onChange={oc}/>);
         expect(editableText.text()).toContain('Hi there!');
+    });
+
+    it('autofocuses the text if it is empty', () => {
+        expect(false).toBe(true);
+    });
+
+    it('focuses the text if focus() is called', () => {
+        expect(false).toBe(true);
+    });
+
+    it('calls onChange if the text is modified', () => {
+        expect(false).toBe(true);
     });
 });
