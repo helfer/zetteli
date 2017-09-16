26f564bfe1eb1e0915e6f8ebabc2c9b41c320a49
Jonas Helfer <jonas@helfer.email>
2017-09-15 19:34:18 -0700

    Add linting

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import * as bodyParser from "body-parser";
+import * as bodyParser from 'body-parser';
 import {
     graphqlExpress,
     graphiqlExpress
@@ -14,6 +14,7 @@ const app = express();
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
+// tslint:disable-next-line no-console-log
 app.listen(PORT, () => console.log(
     `GraphiQL is now running on http://localhost:${PORT}/graphiql`
 ));
