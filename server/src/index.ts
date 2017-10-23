1514c4f51f356dcd5af735a7f6d395249fca081f
Jonas Helfer <jonas@helfer.email>
2017-10-23 10:21:02 -0700

    Chore: Change CORS origin to port 3000

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ const PORT = 3010;
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3020',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 // for IE11 & Co. 
   }
 app.use(cors(corsOptions));
