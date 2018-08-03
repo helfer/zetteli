ef3811e72487a756ddca1894eacabc1a794ae88a
Jonas Helfer <jonas@helfer.email>
2018-08-02 23:37:57 -0700

    Fix the add event log migration

--- server/migrations/20180802224054_event_log.js
+++ server/migrations/20180802224054_event_log.js
@@ -1,28 +1,32 @@
 
 exports.up = function (knex, Promise) {
-    return knex.schema.createTable('log', (table) => {
-        table.bigIncrements('id').primary();
-        table.string('opId');
-        table.string('type');
-        table.dateTime('eventTime');
-        table.integer('eventSchemaId');
-        table.json('payload');
-    });
+    return Promise.all([
+        knex.schema.createTable('log', (table) => {
+            table.bigIncrements('id').primary();
+            table.string('opId');
+            table.string('type');
+            table.dateTime('eventTime');
+            table.integer('eventSchemaId');
+            table.json('payload');
+        }),
 
-    return knex.schema.table('zettelis', function(t) {
-        t.bigInteger('versionId').unsigned().notNull().defaultTo(0);
-    });
-    return knex.schema.table('stacks', function(t) {
-        t.bigInteger('versionId').unsigned().notNull().defaultTo(0);
-    });
+        knex.schema.table('zettelis', function(t) {
+            t.bigInteger('versionId').unsigned().notNull().defaultTo(0);
+        }),
+        knex.schema.table('stacks', function(t) {
+            t.bigInteger('versionId').unsigned().notNull().defaultTo(0);
+        }),
+    ]);
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('log');
-    return knex.schema.table('zettelis', function(t) {
-        t.dropColumn('versionId');
-    });
-    return knex.schema.table('stacks', function(t) {
-        t.dropColumn('versionId');
-    });
+    return Promise.all([
+        knex.schema.dropTable('log'),
+        knex.schema.table('zettelis', function(t) {
+            t.dropColumn('versionId');
+        }),
+        knex.schema.table('stacks', function(t) {
+            t.dropColumn('versionId');
+        }),
+    ]);
 };
